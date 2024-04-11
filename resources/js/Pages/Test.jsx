dae11aeaaddd8006213cfff3e6b7344f00727bf1
josegerryacevedo <josegerryacevedojr@gmail.com>
2024-04-11 15:39:05 +0800

    Set up

---
+++ resources/js/Pages/Test.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+const Test = () => {
+    return (
+        <h1 class="text-4xl font-bold underline bg-red-700">Hello world!</h1>
+    );
+};
+
+export default Test;
