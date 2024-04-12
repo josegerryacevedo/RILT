fc406782c06bac419b4012065e25373221243d03
josegerryacevedo <josegerryacevedojr@gmail.com>
2024-04-12 09:55:29 +0800

    Fixing attributes

--- resources/js/Pages/Test.jsx
+++ resources/js/Pages/Test.jsx
@@ -2,7 +2,9 @@ import React, { useState } from "react";
 
 const Test = () => {
     return (
-        <h1 class="text-4xl font-bold underline bg-red-700">Hello world!</h1>
+        <h1 className="text-4xl font-bold underline bg-red-700">
+            Hello world!
+        </h1>
     );
 };
 
